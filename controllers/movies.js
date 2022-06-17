f862b45698c8bc2f768731acfa67464c4be62477
Michael <msiller455@gmail.com>
2022-06-17 12:34:23 -0700

    finishes up movie create action

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,31 @@
+const Movie = require('../models/movie')
+
 module.exports = {
-    new: newMovie
+    new: newMovie,
+    create
 }
 
 function newMovie(req, res) {
     res.render('movies/new')
+}
+
+function create(req, res) {
+    // convert nowShowing's checkbox of nothing or "on" to boolean
+    req.body.nowShowing = !!req.body.nowShowing
+    // remove any whitespace at start and end of cast
+    req.body.cast = req.body.cast.trim()
+    // split cast into an array if it's not an empty string - using a regular expression as a separator
+    if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/)
+    const movie = new Movie(req.body)
+    movie.save(function(err) {
+        // if we don't redirect, the new page will be shown
+        // with /movies in the address bar 
+        if(err) {
+            console.log(err)
+        
+            return res.redirect('/movies/new')
+        }
+        console.log(movie)
+        res.redirect('/movies/new')
+    })
 }
--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const moviesCtrl = require('../controllers/movies')
 
+// all of our routes are prefixed with /movies because of how they are mounted in the server
 router.get('/new', moviesCtrl.new)
+router.post('/', moviesCtrl.create)
 
 module.exports = router;
